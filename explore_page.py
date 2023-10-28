80ace6ba2e6aa9a9a91c877c2bce4dac7555439b
Phaneendra Bottu <91441682+Phaneendra-Bottu@users.noreply.github.com>
2023-10-28 15:46:48 +0530

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def shorten_categories(categories, cutoff):
     categorical_map = {}
     for i in range(len(categories)):
